f2ec25a2867083bd265ebaaac092e90dbdd9ac69
Stian Soiland-Reyes <soiland-reyes@cs.manchester.ac.uk>
2014-06-18 14:00:04 +0100

    Compact using owl.jsonld as @context

--- src/main/java/no/s11/owlapijsonld/JsonLdRenderer.java
+++ src/main/java/no/s11/owlapijsonld/JsonLdRenderer.java
@@ -3,6 +3,7 @@ package no.s11.owlapijsonld;
 import java.io.IOException;
 import java.io.StringWriter;
 import java.io.Writer;
+import java.net.URL;
 
 import org.coode.owlapi.rdf.model.RDFResourceNode;
 import org.semanticweb.owlapi.model.OWLOntology;
@@ -32,12 +33,16 @@ public class JsonLdRenderer extends RDFRendererAdapter {
 		triples = triples.replace(" -> ", " "); 
 		triples = triples.replace("\n", " .\n");
 		
-		System.out.println("Triples: ");
-		System.out.println(triples);
+//		System.out.println("Triples: ");
+//		System.out.println(triples);
 		JsonLdOptions options = new JsonLdOptions();
-		options.outputForm = "compacted";		
+		
 		try {
-			Object json = JsonLdProcessor.fromRDF(triples, options);			
+			Object json = JsonLdProcessor.fromRDF(triples, options);
+			URL owlJsonLD = getClass().getResource("owl.jsonld");
+			Object context = JsonUtils.fromURL(owlJsonLD);
+			json = JsonLdProcessor.flatten(json, context, options);
+			
 			JsonUtils.writePrettyPrint(writer, json);			
 		} catch (JsonLdError e) {
 			throw new IOException(e);
